81e9854823ce15b73047b96bdddcb4426d292155
snowx92 <jonsnowelshreef92@gmail.com>
2025-09-24 14:58:55 +0300

    fixs

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+# Add Google Chrome repository and install Chrome
+apt-get update
+apt-get install -y wget gnupg2
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install -y google-chrome-stable
